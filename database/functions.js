16867cfa2da2ecf96d26d5ccee6af5384b5e666a
Ap0sentada <70923059+ap0sentada@users.noreply.github.com>
2021-11-12 14:55:12 -0300

    Update functions.js

--- database/functions.js
+++ database/functions.js
@@ -3,7 +3,7 @@ module.exports = (db) => {
         const fs = require('fs')
         const bases = JSON.parse(fs.readFileSync('./database/base_values.json', "utf-8"))
         let x = await db.get("storage", `${user_id}_${key}`);
-        if (x === undefined) x = eval(`bases.${key}`);
+        if (x === undefined) x = bases[key];
         else x = x.value;
         if (x === undefined) return `${key} não foi préviamente definido em \`base_values.json\``
         return x
